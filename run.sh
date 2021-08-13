945c34b660d98d7afa45963bea7358d2d6106516
Ben Schumacher <me@benschumacher.com>
2021-08-13 19:15:24 +0000

    Change how stats interval gets random jitter

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 set -e
 
+RANDOM="$(od -vAn -N4 -tu4 < /dev/urandom)"
+
 S3QL_AUTHFILE="${S3QL_AUTHFILE:-/etc/s3ql.authinfo2}"
 S3QL_OPTS="${S3QL_OPTS:-}"
 S3QL_LOG_OUTPUT="${S3QL_LOG_OUTPUT:-/var/log/s3ql.log}"
+
+S3QL_STAT_SECS="${S3QL_STAT_PERIOD:-$((RANDOM % 300 + 1650))}"
+# alternate
+#S3QL_STAT_SECS="${S3QL_STAT_SECS:-$((1800 + $(od -vAn -N1 -td1 < /dev/urandom)))}"
+
 S3QL_STORAGE_URL="$1"
 S3QL_MOUNTPOINT="$2"
 
